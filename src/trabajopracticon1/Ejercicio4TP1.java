6c388d48006653955520cc28d5da7f66b6974258
ariel <ariel@ArielG>
2025-04-19 17:36:00 -0300

    Ejercicio4TP1

--- src/trabajopracticon1/Ejercicio4TP1.java
+++ src/trabajopracticon1/Ejercicio4TP1.java
@@ -12,18 +12,32 @@ import java.util.Scanner;
  */
 public class Ejercicio4TP1 {
 
-    public static void main(String[] args) {
-        Scanner entrada= new Scanner(System.in);
+     public static void main(String[] args) {
+        Scanner entrada=new Scanner(System.in);
         
-        System.out.print("Ingrese el numero: ");
-        int num=entrada.nextInt();
+        System.out.print("Ingrese el dia: ");
+        int dia=entrada.nextInt();
+        System.out.print("Ingrese el mes: ");
+        int mes=entrada.nextInt();
+        System.out.print("Ingrese el anio: ");
+        int anio=entrada.nextInt();
+        System.out.println(+dia+"-"+mes+"-"+anio);
         
-        if(num>=10){
-            System.out.print("El numero tiene dos digitos");
+        if(mes>0 && mes<=3){
+            System.out.println("Primer trimestre del anio");
         }
-        if(num<10){
-            System.out.print("El numero tiene un digito");
-        }    
+        else if(mes>=4 && mes <=6){
+            System.out.println("Segundo trimestre del anio");
+        }
+        else if(mes>=7 && mes <=9){
+            System.out.println("Tercer trimestre del anio");
+        }
+        else if(mes>=10 && mes <=12){
+            System.out.println("Cuarto trimestre del anio");
+        }
+        else{
+            System.out.println("Valor incorrecto");
+        }
     }
     
 }
